2d79f1d88613382ccba85d83ececb1b7c05142ed
Justin Gabriele <gabrielejustin@yahoo.com>
2020-09-21 17:39:29 -0400

    added an array for uppercase letters and added prompts/confirms for input variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,14 @@
 // user input variables
-var passLength;
-var isNumber;
-var isSpecial;
-var isUpper;
-var isLower;
+var passLength = prompt("How long do you want your password to be? Password must be between 8 and 128 characters.");
+var isNumber = confirm("Do you want numbers in your password?");
+var isSpecial = confirm("Do you want special characters in your password?");
+var isUpper = confirm("Do you want uppercase letters in your password?");
+var isLower = confirm("Do you want lowercase letters in your password?");
 
 // arrays holding possible variable values
 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
